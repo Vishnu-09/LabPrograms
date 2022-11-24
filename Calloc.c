e18fd183147827cd624369cd8b5cdf8df542b0d3
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2022-11-24 23:54:28 +0530

    new

--- Calloc.c
+++ Calloc.c
@@ -1,4 +1,20 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+   {
+
+       int i,n,s=0,*p;
+       printf("enter the size");
+       scanf("%d",&n);
+       p=(int *)calloc(n,sizeof(int));
+       printf("enter elements");
+       for(i=0;i<n;i++)
+       {
+
+           scanf("%d",p+i);
+           s = s+ *(p+i);
+       }
        printf("sum=%d",s);
        free(p);
        return 0;
